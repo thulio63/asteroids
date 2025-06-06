d4230aa5d7506e00ff11b5711082570aed61717f
thulio63 <thulio63@gmail.com>
2025-06-06 13:34:40 -0500

    extended pygame Sprite class

--- circleshape.py
+++ circleshape.py
@@ -1,6 +1,3 @@
-import pygame
-from constants import *
-
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
@@ -20,23 +17,3 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # overridden by sub-classes
         pass
-
-
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
-    dt = 0
-    
-    
-    while True: # Game loop
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        screen.fill("black")
-        pygame.display.flip()
-        #60 FPS
-        dt = clock.tick(60) / 1000
-
-if __name__ == "__main__":
-    main()
