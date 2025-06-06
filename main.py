31aa01b58f611707304c439d7637f2f6eea22403
thulio63 <thulio63@gmail.com>
2025-06-06 14:09:09 -0500

    finished player movement, created object groups in main.py

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True: # Game loop
@@ -14,9 +19,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
-        
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         #60 FPS
         dt = clock.tick(60) / 1000
--- player.py
+++ player.py
@@ -1,11 +1,12 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
 class Player(CircleShape):
     def __init__(self, x, y, radius = PLAYER_RADIUS):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        #self.containers = (drawable, updatable)
         
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -21,10 +22,18 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
         
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+        
     def update(self, dt):
         keys = pygame.key.get_pressed()
         
+        if keys[pygame.K_w]:
+            self.move(dt)
         if keys[pygame.K_a]:
             self.rotate(dt * -1)
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
         if keys[pygame.K_d]:
             self.rotate(dt)
