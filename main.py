0ca7cd37c6baea373dcfd7b4c84ef29a27a641f0
thulio63 <thulio63@gmail.com>
2025-06-06 13:21:12 -0500

    functional game window

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import pygame
+from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    while True:
+        screen.fill("black")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
